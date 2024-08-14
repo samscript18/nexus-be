3174311c193220e4eac1af6c49eb0e82bdc690e3
Adesugba Samuel <adeolasam20@gmail.com>
2024-08-14 16:27:23 +0100

    feat: cors update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://nexus-2024.vercel.app',
+  ];
+
+  app.enableCors({ origin: allowedOrigins, credentials: true });
+  app.use(helmet());
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
